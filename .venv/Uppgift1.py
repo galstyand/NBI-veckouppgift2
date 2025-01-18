3bad651adb2b089ba41f36943d9a18ded8e7cd59
davidgalstyancaspeco <david.galstyan@caspeco.se>
2025-01-18 21:27:39 +0100

    balder

---
+++ .venv/Uppgift1.py
@@ -0,0 +1,16 @@
+is_member = False
+level1 = 100
+level2 = 300
+discount = 0
+
+price = input("Välkommen, köp något dyrt: ")
+price = float(price)
+if level2 > price > level1:
+    print("Grattis! Du har avancerat till nivå 1 och får 10% rabatt.")
+    discount = discount + 10
+if price >= level2:
+    print("Grattis! Du har avancerat till nivå 2 och får 25% rabatt.")
+    discount = discount + 25
+
+final_price = price * (100 - discount) / 100
+print("Efter rabatter blir priset.... " + str(final_price))
